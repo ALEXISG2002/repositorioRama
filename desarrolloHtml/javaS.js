cfeb85cf23f744d33415e7c6078284d3ebd6ee50
Alexis <guelesix@hotmail.com>
2023-11-15 09:52:41 -0500

    antepenultimo

---
+++ desarrolloHtml/javaS.js
@@ -0,0 +1,19 @@
+import './styles.css';
+
+function generarNumeroAleatorio(){
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function dibujarDAdos(){
+    const dados= document.querySelectorAll('.dado');
+
+    for (let i = 0; i < dados.length; i++){
+        const dado=[i];
+        const imagen = document.createElement('img');
+        imagen.src ='img/dado-${generarNumeroAleatorio()}.png';
+    
+    }
+
+}
+
+document.querySelector('#lanzar').addEventListener('click', () =>{});
