b13be49fb478f4e3bf7cadb8cbdc052354c6de13
Alexis <guelesix@hotmail.com>
2023-11-15 11:09:22 -0500

    vs 1.3

--- desarrolloHtml/javaS.js
+++ desarrolloHtml/javaS.js
@@ -1,19 +1,29 @@
-import './rama.css';
+// Importamos los archivos CSS
 
-function generarNumeroAleatorio(){
+
+// Creamos una función para generar un número aleatorio entre 1 y 6
+function generarNumeroAleatorio() {
     return Math.floor(Math.random() * 6) + 1;
-}
-
-function dibujarDAdos(){
-    const dados= document.querySelectorAll('.dado');
-
-    for (let i = 0; i < dados.length; i++){
-        const dado=[i];
-        const imagen = document.createElement('img');
-        imagen.src ='img/dado-${generarNumeroAleatorio()}.png';
-    
+  }
+  
+  // Creamos una función para dibujar los dados
+  function dibujarDados() {
+    // Obtenemos los dados
+    const dados = document.querySelectorAll('.dado');
+  // Generamos un número aleatorio para cada dado
+    for (let i = 0; i < dados.length; i++) {
+      const dado = dados[i];
+  
+      // Cargamos la imagen del dado
+      const imagen = document.createElement('img');
+      imagen.src = `dado-${generarNumeroAleatorio()}.png`;
+  
+      // Agregamos la imagen al dado
+      dado.appendChild(imagen);
     }
+  }
+  dibujarDados();
 
-}
-
-document.querySelector('#lanzar').addEventListener('click', () =>{});
+  document.querySelector('#lanzar').addEventListener('click', ()=>
+  {dibujarDados
+});
